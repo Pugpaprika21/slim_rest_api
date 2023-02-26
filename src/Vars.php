61d8643a3c6741ba1372a5c8f6e41bea312bc1a2
Pugpaprika21 <pugpaprika21@gmail.com>
2023-02-26 04:17:46 -0800

    e

--- src/Vars.php
+++ src/Vars.php
@@ -4,26 +4,65 @@ namespace Includer;
 
 class Vars
 {
-    private static $dd;
+    private const DUMP_R_OPTION = 1;
+    private const DUMP_V_OPTION = 2;
+
     /**
      * #Vars::dump($args);
      *
      * @param mixed $args
      * @param boolean $exist
+     * @param int $dumps_opt
      * @return void
      */
-    public static function dump(mixed $args, bool $exist = false): void
+    public static function dump(mixed $args, bool $exist = false, int $dumps_opt = self::DUMP_R_OPTION): void
     {
-        $dumps = function (mixed $args, bool $exist = false) {
+        if ($dumps_opt == 1) {
+            self::dumpsArray($args, $exist);
+        } else {
+            self::dumpsMixed($args, $exist);
+        }
+    }
+
+    /**
+     * @param mixed $args
+     * @param boolean $exist
+     * @return void
+     */
+    private static function dumpsArray(mixed $args, bool $exist = false): void
+    {
+        $dArrayFunc = function () use ($args, $exist) {
             echo "<pre>";
             echo "<div id='debug-data' style='padding: 10px; color: #FFFFFF; background-color: #000000;'>";
             print_r($args);
             echo "</div>";
             echo "</pre>";
 
-            if ($exist) exit;
+            if ($exist) {
+                exit;
+            }
         };
+        $dArrayFunc();
+    }
 
-        $dumps($args, $exist);
+    /**
+     * @param mixed $args
+     * @param boolean $exist
+     * @return void
+     */
+    private static function dumpsMixed(mixed $args, bool $exist = false): void
+    {
+        $dMixedFunc = function () use ($args, $exist) {
+            echo "<pre>";
+            echo "<div id='debug-data' style='padding: 10px; color: #FFFFFF; background-color: #000000;'>";
+            var_dump($args);
+            echo "</div>";
+            echo "</pre>";
+
+            if ($exist) {
+                exit;
+            }
+        };
+        $dMixedFunc();
     }
 }
