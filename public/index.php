eb0f516f8c4313f61d6923ecbfdff7f57a23a7f9
Pugpaprika21 <pugpaprika21@gmail.com>
2023-03-01 04:52:26 -0800

    e

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 use App\Controller\UserController;
 use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -12,4 +14,11 @@ $app->addRoutingMiddleware();
 $app->get('/show_users', UserController::class . ':showUsers')->setName('user');
 $app->get('/show_user/{id}', UserController::class . ':showUser')->setName('users');
 
+$app->get('/provider_init', function (Request $request, Response $response, array $args) {
+    return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
+});
+
 $app->run();
+
+
+
